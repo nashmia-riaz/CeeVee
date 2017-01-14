64d993c3ee16c7aca064757239fa47579639ed8b
thenash654 <nashmia-riaz@hotmail.com>
2017-01-14 17:26:27 +0500

    + button fixed

--- js/dynamicpage.js
+++ js/dynamicpage.js
@@ -32,31 +32,30 @@ $(function() {
                         $("nav a[href="+newHash+"]").addClass("current");
                     });
                 });
-        };
+        }
 
     });
 
     $(window).trigger('hashchange');
 
 });
+//
+// var room = 1;
+// function add_fields() {
+//     room++;
+//     var objTo = document.getElementById('room_fields')
+//     var divtest = document.createElement("div");
+//     divtest.innerHTML = '<label class="degree">Degree</label><br><input type="text" id="degree" name="degree"><br><label class="grad">Year of Graduation</label><br><input type="text" id="grad" name="year of graduation"><br><label class="grade">Grade/CGPA</label><br><input type="text" id="grade" name="grade"><br><label class="school">School/College</label><br><input type="text" id="school" name="school"><br>'
+//
+//      objTo.appendChild(divtest)
+// }
 
 var room = 1;
 function add_fields() {
     room++;
-    var objTo = $("#room_fields");
-    // var divtest = document.createElement("div");
-    var divtest = '<hr style="width:100%;"><div><label class="degree">Degree</label><br><input type="text" id="degree" name="degree"><br><label class="grad">Year of Graduation</label><br><input type="text" id="grad" name="year of graduation"><br><label class="grade">Grade/CGPA</label><br><input type="text" id="grade" name="grade"><br><label class="school">School/College</label><br><input type="text" id="school" name="school"><br></div>';
-    // divtest.innerHTML = '<label class="degree">Degree</label><br><input type="text" id="degree" name="degree"><br><label class="grad">Year of Graduation</label><br><input type="text" id="grad" name="year of graduation"><br><label class="grade">Grade/CGPA</label><br><input type="text" id="grade" name="grade"><br><label class="school">School/College</label><br><input type="text" id="school" name="school"><br>';
+    var objTo = document.getElementById('meow');
+    var divtest = document.createElement("div");
+    divtest.innerHTML = ' <hr style="width:100%;"><label class="company">Company</label><br><input type="text" id="company" name="company"><br><label class="start">Starting Date</label><br><input type="date" id="start" name="starting date"><br><label class="end">End Date</label><br><input type="text" id="end" name="end date"><br><label class="pos">Designation</label><br><input type="text" id="pos" name="designation"><br>';
     $("#page-wrap").css("height","100%");
-     objTo.append(divtest);
+    objTo.appendChild(divtest);
 }
-
-var room = 2;
-// function add_fields() {
-//     room++;
-//     var objTo = document.getElementById('meow')
-//     var divtest = document.createElement("div");
-//     divtest.innerHTML = ' <label class="company">Company</label><br><input type="text" id="company" name="company"><br><label class="start">Starting Date</label><br><input type="date" id="start" name="starting date"><br><label class="end">End Date</label><br><input type="text" id="end" name="end date"><br><label class="pos">Designation</label><br><input type="text" id="pos" name="designation"><br>'
-//
-//      objTo.appendChild(divtest)
-// }
